72098f0d06cff08a9cf1e216306607d0506101ee
Maicol Salazar <stivsal12@gmail.com>
2024-06-09 09:31:08 -0500

    advances

--- src/pages/AnimesPage.jsx
+++ src/pages/AnimesPage.jsx
@@ -9,13 +9,12 @@ const AnimesPage = () => {
 
 
     useEffect(() => {
-
-        const url = `https://api.jikan.moe/v4/anime` 
-        axios
-    .get(url)
-    .then(response => {
-      console.log('aqui ahi: ', response.data.data)
-      setAnimes(response.data) 
+     const url = `https://api.jikan.moe/v4/anime` 
+     axios
+      .get(url)
+      .then(response => {
+      console.log('datos recibidos: ', response.data.data)
+      setAnimes(response.data.data) 
     
     })
     .catch(error => {
@@ -26,7 +25,18 @@ const AnimesPage = () => {
 
 
   return (
-    <div>AnimesPage</div>
+    <div>
+        <h1>AnimesPage</h1>
+    <figure>
+    {
+        animes.map(anime => 
+            <img className='anime_img' key={anime.mal_id} src={anime.images.jpg.image_url}>
+            </img>
+            
+        )
+    }
+    </figure>
+    </div>
   )
 }
 
