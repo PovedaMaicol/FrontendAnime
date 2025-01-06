054d87d3707b4ec56f4ec82f36e36e415de9a3d2
Maicol Salazar <stivsal12@gmail.com>
2025-01-06 18:31:49 -0500

    styles

--- src/pages/AnimeId.jsx
+++ src/pages/AnimeId.jsx
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import useFetch from '../hooks/useFetch';
+import './styles/animeId.css';
 
 const AnimeId = () => {
   const { id } = useParams(); // Capturo el id del anime desde la URL
@@ -9,31 +10,78 @@ const AnimeId = () => {
   useEffect(() => {
     const url = `https://api.jikan.moe/v4/anime/${id}`;
     fetchData(url); // Llamar a la función fetchData con la URL específica
+    console.log('el anime es:', animeId); // Verificar que el anime se cargó correctamente  
   }, [id]);
 
   if (loading) return <p>Cargando anime...</p>;
   if (error) return <p>Hubo un error al cargar el anime</p>;
 
+  // Verificar si animeId y las propiedades existen antes de renderizarlas
+  if (!animeId || !animeId.genres) {
+    return <p>No se pudo cargar la información del anime.</p>;
+  }
+
   return (
-    <div>
-      <h1>{animeId.title}</h1>
+    <div style={{ padding: '15px' }}>
+
+
+      <section className="first-section">
+        <img
+          src={animeId.images?.webp?.large_image_url}
+          alt={animeId.title}
+          className="container-img"
+        />
+
+        <div className="first-section-info">
+          <h2>{animeId.title}</h2>
+
+          <div className="genres">
+           
+            {animeId.genres.map((genre) => (
+              <span key={genre.mal_id} className="genre">
+                {genre.name}
+              </span>
+            ))}
+          </div>
+          <br/>
+
+          <table className="anime-info-table">
+        <thead>
+          <tr>
+            <th>Atributo</th>
+            <th>Valor</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Episodios</td>
+            <td>{animeId.episodes}</td>
+          </tr>
+          <tr>
+            <td>Puntaje</td>
+            <td>{animeId.score}</td>
+          </tr>
+          <tr>
+            <td>Rango</td>
+            <td>{animeId.rank}</td>
+          </tr>
+          <tr>
+            <td>Miembros</td>
+            <td>{animeId.members}</td>
+          </tr>
+          <tr>
+            <td>Estado</td>
+            <td>{animeId.status}</td>
+          </tr>
+        </tbody>
+          </table>
+
+        </div>
+      </section>
+<br/>
       <p>{animeId.synopsis}</p>
-      <p>{animeId.episodes} episodes</p>
-      <p>{animeId.score} score</p>
-      <p>{animeId.status}</p>
-      <p>{animeId.rank} rank</p>
-      <p>{animeId.members} members</p>
-      <div>
-        {animeId.genres.map((genre) => (
-          <span key={genre.mal_id} style={{ marginRight: '10px' }}>
-            {genre.name}
-          </span>
-        ))}
-      </div>
 
-
-
-      <img src={animeId.images.webp.large_image_url} alt={animeId.title} />
+      
     </div>
   );
 };
