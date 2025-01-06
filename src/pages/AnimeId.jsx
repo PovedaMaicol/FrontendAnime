7ab469f57aaeababc8094becf72353e3d1a23e62
Maicol Salazar <stivsal12@gmail.com>
2025-01-05 23:44:13 -0500

    cardAnimeId

--- src/pages/AnimeId.jsx
+++ src/pages/AnimeId.jsx
@@ -1,17 +1,16 @@
-import React, {useState, useEffect} from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import useFetch from '../hooks/useFetch';
 
 const AnimeId = () => {
-
-  const { id } = useParams(); // capturo el id del anime desde la url 
-  const { data: animeId, fetchData, loading, error } = useFetch();    // Obtener la data del anime específico 
+  const { id } = useParams(); // Capturo el id del anime desde la URL
+  const { data: animeId, fetchData, loading, error } = useFetch(); // Obtener la data del anime específico
 
   useEffect(() => {
     const url = `https://api.jikan.moe/v4/anime/${id}`;
     fetchData(url); // Llamar a la función fetchData con la URL específica
   }, [id]);
-  
+
   if (loading) return <p>Cargando anime...</p>;
   if (error) return <p>Hubo un error al cargar el anime</p>;
 
@@ -19,7 +18,22 @@ const AnimeId = () => {
     <div>
       <h1>{animeId.title}</h1>
       <p>{animeId.synopsis}</p>
-      {/* <img src={animeId.images.jpg.image_url} alt={animeId.title} /> */}
+      <p>{animeId.episodes} episodes</p>
+      <p>{animeId.score} score</p>
+      <p>{animeId.status}</p>
+      <p>{animeId.rank} rank</p>
+      <p>{animeId.members} members</p>
+      <div>
+        {animeId.genres.map((genre) => (
+          <span key={genre.mal_id} style={{ marginRight: '10px' }}>
+            {genre.name}
+          </span>
+        ))}
+      </div>
+
+
+
+      <img src={animeId.images.webp.large_image_url} alt={animeId.title} />
     </div>
   );
 };
